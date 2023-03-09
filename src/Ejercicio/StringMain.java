ab93d717b08301182483616c11d2f6c858b9c165
Hyrum Carrasco <hyrumcarrasco7@gmail.com>
2023-03-09 18:30:57 -0400

    Java basico

---
+++ src/Ejercicio/StringMain.java
@@ -0,0 +1,22 @@
+package Ejercicio;
+
+public class StringMain {
+
+    public static void main(String[] args) {
+
+        int sumar = 5 + 10;
+
+        System.out.println(sumar);
+
+        long dividir = 20 / 6;
+
+        System.out.println(dividir);
+
+        String nombre = "Agustin";
+        String apellido = "Carrasco";
+
+        System.out.println("El nombre de mi bebe sera " + nombre +" " + apellido);
+
+
+    }
+}
---
+++ src/Ejercicio/TiposMain.java
@@ -0,0 +1,26 @@
+package Ejercicio;
+
+public class TiposMain {
+
+    public static void main(String[] args) {
+
+        boolean verificador = 20 > 50;
+
+        int num1 = 28;
+        int num2 = 20;
+        int num3 = 50;
+        int num4 = 120;
+
+
+        if (num1 < num2 && num2 < num3) {
+
+            System.out.println("verdadero");
+        } else if (num3 < num4) {
+            System.out.println("Else If");
+        } else {
+        }
+        System.out.println("Fiiiin ");
+
+    }
+
+}
